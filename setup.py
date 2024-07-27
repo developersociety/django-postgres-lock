5f21b0bb2d90c9bae6da22ee308e6d92461d9067
Alex Tomkins <tomkins@darkzone.net>
2024-07-27 21:21:32 +0100

    Drop support for Python 3.7

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     platforms=["any"],
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=["Django>=2.2"],
     classifiers=[
         "Intended Audience :: Developers",
@@ -29,7 +29,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
